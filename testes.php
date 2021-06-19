8f9f3a1cd18a21e7c3cff47a4051ceea0436f59b
Renato Augusto <renatoaugusto.ads@gmail.com>
2021-06-19 13:20:48 -0300

    Implementando o padrão Template Method. Classes IKCV & ICPP foram criadas e possuem caracteristicas iguais, pois possuem duas aliquotas diferentes dos impostos convencionais que só possuem uma, entretanto continuam sendo impostos, então abstraimos os códigos iguais para uma classe abstrata com um método final publico que não pode ser sobrescrito e quebramos o algoritmo em pequenos métodos que serão executados dentro deste metodo final

--- testes.php
+++ testes.php
@@ -4,19 +4,29 @@ require_once "vendor/autoload.php";
 
 use Alura\DesignPattern\CalculadoraDeDescontos;
 use Alura\DesignPattern\CalculadoraDeImpostos;
+use Alura\DesignPattern\Impostos\Icpp;
+use Alura\DesignPattern\Impostos\Ikcv;
 use Alura\DesignPattern\Orcamento;
 use Alura\DesignPattern\Impostos\Icms;
 use Alura\DesignPattern\Impostos\Iss;
 
 
 $orcamento = new Orcamento();
-$orcamento->valor = 1000;
+$orcamento->valor = 300;
 $orcamento->quantidadeDeItens = 6;
 
-/*
+
 $calculadoraDeImpostos = new CalculadoraDeImpostos();
-echo $calculadoraDeImpostos->calculaImposto($orcamento, new Icms());
-*/
+echo $calculadoraDeImpostos->calculaImposto($orcamento, new Iss()) . PHP_EOL;
+
+$calculadoraDeImpostos = new CalculadoraDeImpostos();
+echo $calculadoraDeImpostos->calculaImposto($orcamento, new Icms()) . PHP_EOL;
+
+$calculadoraDeImpostos = new CalculadoraDeImpostos();
+echo $calculadoraDeImpostos->calculaImposto($orcamento, new Ikcv()) . PHP_EOL;
+
+$calculadoraDeImpostos = new CalculadoraDeImpostos();
+echo $calculadoraDeImpostos->calculaImposto($orcamento, new Icpp()) . PHP_EOL;
 
 $calculadoraDeDescontos = new CalculadoraDeDescontos();
-echo $calculadoraDeDescontos->CalculaDesconto($orcamento);
+echo $calculadoraDeDescontos->CalculaDesconto($orcamento) . PHP_EOL;
