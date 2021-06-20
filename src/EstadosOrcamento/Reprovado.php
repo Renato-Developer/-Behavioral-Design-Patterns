1e2136a763fa6fcfcab066009405773de239cfe4
Renato Augusto <renatoaugusto.ads@gmail.com>
2021-06-20 00:00:59 -0300

    Implementando o Padrão State (Adicionando novo módulo de Estado de um Orçamento, que é a extração desta regra de negócio para classes que implementam uma abstração e que representam um estado deste Orçamento)

---
+++ src/EstadosOrcamento/Reprovado.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Alura\DesignPattern\EstadosOrcamento;
+
+
+use Alura\DesignPattern\Orcamento;
+
+class Reprovado extends EstadoOrcamento
+{
+    public function calculaDescontoExtra(Orcamento $orcamento): float
+    {
+        throw new \DomainException('Um orçamento Reprovado não pode receber desconto');
+    }
+
+    public function finaliza(Orcamento $orcamento)
+    {
+        $orcamento->estadoAtual = new Finalizado();
+    }
+}
--- src/Orcamento.php
+++ src/Orcamento.php
@@ -2,8 +2,37 @@
 
 namespace Alura\DesignPattern;
 
-Class Orcamento 
+use Alura\DesignPattern\EstadosOrcamento\EmAprovacao;
+use Alura\DesignPattern\EstadosOrcamento\EstadoOrcamento;
+
+Class Orcamento
 {
     public float $valor;
     public int $quantidadeDeItens;
+    public EstadoOrcamento $estadoAtual;
+
+    public function __construct()
+    {
+        $this->estadoAtual = new EmAprovacao();
+    }
+
+    public function aplicaDescontoExtra()
+    {
+        $this->valor -= $this->estadoAtual->calculaDescontoExtra($this);
+    }
+
+    public function aprova()
+    {
+        $this->estadoAtual->aprova($this);
+    }
+
+    public function reprova()
+    {
+        $this->estadoAtual->reprova($this);
+    }
+
+    public function finaliza()
+    {
+        $this->estadoAtual->finaliza($this);
+    }
 }
