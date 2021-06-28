0e22e429538ad84995320728b975427eb63a87eb
Renato Augusto <renatoaugusto.ads@gmail.com>
2021-06-27 22:31:16 -0300

    Implementando o padrão Abstract Factory que é muito falado e é bem parecido com o template Method

---
+++ src/Log/FileLogWritter.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Alura\DesignPattern\Log;
+
+
+class FileLogWritter implements LogWritter
+{
+    private string $arquivo;
+
+    public function __construct(string $caminhoArquivo)
+    {
+        $this->arquivo = fopen($caminhoArquivo, 'a+');
+    }
+
+    public function escreve(string $mensagemFormatada): void
+    {
+        fwrite($this->arquivo, $mensagemFormatada);
+    }
+
+    public function __destruct()
+    {
+        fclose($this->arquivo);
+    }
+}
---
+++ src/Log/StdoutLogWritter.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Alura\DesignPattern\Log;
+
+
+class StdoutLogWritter implements LogWritter
+{
+    public function escreve(string $mensagemFormatada): void
+    {
+        echo $mensagemFormatada;
+    }
+}
